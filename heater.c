5addca0284b4b488d5755c6ce92bf3613c432a79
David Forrest <drf@vims.edu>
2014-02-09 10:40:04 -0500

    heater.c: Add units comments and sum the PID in consistent units, changing internal kP,kI,kD meanings.

--- heater.c
+++ heater.c
@@ -92,21 +92,21 @@ struct {
 #ifdef PID_P
 #define         DEFAULT_P                               PID_P
 #else
-#define		DEFAULT_P				8192
+#define		DEFAULT_P				(32*PID_SCALE)
 #endif 
 
 /// default PID_SCALE scaled I factor, equivalent to 0.5 ( 0.5 counts/((C/4)*(s/4)) or 8 counts/C/s
 #ifdef PID_I
 #define         DEFAULT_I                               PID_I
 #else
-#define		DEFAULT_I				512
+#define		DEFAULT_I				(8*PID_SCALE)
 #endif
 
 /// default PID_SCALE scaled D factor, equivalent to 24 ( 24 counts/((C/4)/(TH_COUNT*s/4) or 24*TH_COUNT counts/(C/s)
 #ifdef PID_D
 #define         DEFAULT_D                               PID_D
 #else
-#define		DEFAULT_D				24576
+#define		DEFAULT_D				(24*TH_COUNT*PID_SCALE)
 #endif
 
 /// default I limit, in units of (C/4)*(s*4) or C*s
@@ -336,26 +336,26 @@ void heater_tick(heater_t h, temp_type_t type, uint16_t current_temp, uint16_t t
 
 		// PID stuff
 		// proportional
-		heater_p = t_error;
+		heater_p = t_error;   //  units: 1C=4qC
 
 		// integral
-		heaters_runtime[h].heater_i += t_error;
+		heaters_runtime[h].heater_i += t_error;  // units: 1C*s=16qC*qs
 		// prevent integrator wind-up
 		if (heaters_runtime[h].heater_i > heaters_pid[h].i_limit)
 			heaters_runtime[h].heater_i = heaters_pid[h].i_limit;
 		else if (heaters_runtime[h].heater_i < -heaters_pid[h].i_limit)
 			heaters_runtime[h].heater_i = -heaters_pid[h].i_limit;
 
-		// derivative: dT/dt as C/4 per (TH_COUNT*s/4)
+		// derivative // units: 1C/s=TH_COUNT*4qC/4qs=8qC/qs
 		// note: D follows temp rather than error so there's no large derivative when the target changes
 		heater_d = heaters_runtime[h].temp_history[heaters_runtime[h].temp_history_pointer] - current_temp;
 
 		// combine factors
 		int32_t pid_output_intermed = (
 			(
-				(((int32_t) heater_p) * heaters_pid[h].p_factor) +
-				(((int32_t) heaters_runtime[h].heater_i) * heaters_pid[h].i_factor) +
-				(((int32_t) heater_d) * heaters_pid[h].d_factor)
+				(((int32_t) heater_p) * heaters_pid[h].p_factor)/4 +                      //   C * kP
+				(((int32_t) heaters_runtime[h].heater_i) * heaters_pid[h].i_factor)/16 +  // C*s * kI
+				(((int32_t) heater_d) * heaters_pid[h].d_factor)/TH_COUNT                 // C/s * kD
 			) / PID_SCALE
 		);
 
